56af78251f8ad8de114afec0198e42a9649c9e76
Jaime Galiana <jaimegaliana@iMac-23.local>
2023-09-18 09:33:35 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ejemplo2 Guardar en lista"
+include(":app")
